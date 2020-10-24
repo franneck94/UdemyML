06f71ec7997e150e57bc96f02acbc30ee0401c82
Jan Schaffranek <jan.schaffranek@rub.de>
2020-10-24 14:15:06 +0200

    Update

--- Chapter13_CaseStudies/CaseStudyWebServer/request.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-# URL
-url = 'http://localhost:5000/api'
-
-# Change the value of experience that you want to test
-r = requests.post(url,json={'x': [1, 0, 0, 0, 40, 10, 10]})
-print(r.json())
--- Chapter13_CaseStudies/CaseStudyWebServer/server.py
+++ Chapter13_CaseStudies/CaseStudyWebServer/server.py
@@ -1,17 +1,24 @@
-# Import libraries
-import numpy as np
-from flask import Flask, request, jsonify
+import os
 import pickle
 
-app = Flask(__name__)
-model = pickle.load(open('model.pkl','rb'))
+from flask import Flask, request, jsonify
+import numpy as np
 
-@app.route('/api',methods=['POST'])
+
+PATH = os.path.abspath(os.path.join(__file__, os.pardir))
+MODEL_PATH = os.path.join(PATH, "model.pkl")
+
+APP = Flask(__name__)
+MODEL = pickle.load(open(MODEL_PATH, "rb"))
+
+
+@APP.route('/api', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(data['x'])])
-    output = prediction[0]
+    pred = MODEL.predict([np.array(data['x'])])
+    output = pred[0]
     return jsonify(output)
 
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    APP.run(port=5000, debug=True)
