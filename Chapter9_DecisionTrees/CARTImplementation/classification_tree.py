d5d7825ecddfc7b59f99f61332e07b9f2d1a32cd
Jan Schaffranek <jan.schaffranek@rub.de>
2020-11-04 16:41:42 +0100

    Updated Chapter 7

--- Chapter9_DecisionTrees/CARTImplementation/classification_tree.py
+++ Chapter9_DecisionTrees/CARTImplementation/classification_tree.py
@@ -1,30 +1,42 @@
+from __future__ import annotations
+
 import math
+from typing import Optional
+from typing import SupportsFloat
 
 import numpy as np
 
 
-def split_func(sample, feature_i, threshold):
+def split_func(
+    sample: np.ndarray,
+    feature_i: int,
+    threshold: float
+) -> bool:
     return sample[feature_i] >= threshold
 
 
-def divide_on_feature(X, feature_i, threshold):
-    """ Divide dataset based on if sample value on feature index is larger than
-        the given threshold.
+def divide_on_feature(
+    x: np.ndarray,
+    feature_i: int,
+    threshold: float
+) -> np.ndarray:
+    """Divide dataset based on if sample value on feature index is larger than
+    the given threshold.
     """
     X_1 = np.array(
-        [sample for sample in X if split_func(sample, feature_i, threshold)]
+        [sample for sample in x if split_func(sample, feature_i, threshold)]
     )
     X_2 = np.array(
-        [sample for sample in X if not split_func(sample, feature_i, threshold)]
+        [sample for sample in x if not split_func(sample, feature_i, threshold)]
     )
     return np.array([X_1, X_2])
 
 
-def log2(x):
+def log2(x: SupportsFloat) -> float:
     return math.log(x) / math.log(2)
 
 
-def calculate_entropy(y):
+def calculate_entropy(y: np.ndarray) -> float:
     unique_labels = np.unique(y)
     entropy = 0.0
     for label in unique_labels:
@@ -34,7 +46,11 @@ def calculate_entropy(y):
     return entropy
 
 
-def calculate_information_gain(y, y1, y2):
+def calculate_information_gain(
+    y: np.ndarray,
+    y1: np.ndarray,
+    y2: np.ndarray
+) -> float:
     p = len(y1) / len(y)
     entropy = calculate_entropy(y)
     info_gain = entropy - p * \
@@ -43,8 +59,8 @@ def calculate_information_gain(y, y1, y2):
     return info_gain
 
 
-def majority_vote(y):
-    most_common = None
+def majority_vote(y: np.ndarray) -> float:
+    most_common = 0.0
     max_count = 0
     for label in np.unique(y):
         count = len(y[y == label])
@@ -54,78 +70,78 @@ def majority_vote(y):
     return most_common
 
 
-class DecisionNode():
-    """Class that represents a decision node or leaf in the decision tree
-    Parameters:
-    -----------
-    feature_i: int
-        Feature index which we want to use as the threshold measure.
-    threshold: float
-        The value that we will compare feature values at feature_i against to
-        determine the prediction.
-    value: float
-        The class prediction if classification tree, or float value if regression tree.
-    true_branch: DecisionNode
-        Next decision node for samples where features value met the threshold.
-    false_branch: DecisionNode
-        Next decision node for samples where features value did not meet the threshold.
+class DecisionNode:
+    """Class that represents a decision node or leaf in the decision tree.
     """
 
-    def __init__(self, feature_i=None, threshold=None,
-                 value=None, true_branch=None, false_branch=None):
-        self.feature_i = feature_i          # Index for the feature that is tested
-        self.threshold = threshold          # Threshold value for feature
-        self.value = value                  # Value if the node is a leaf in the tree
-        self.true_branch = true_branch      # 'Left' subtree
-        self.false_branch = false_branch    # 'Right' subtree
+    def __init__(
+        self,
+        feature_i: int = None,
+        threshold: float = None,
+        value: float = None,
+        true_branch: DecisionNode = None,
+        false_branch: DecisionNode = None
+    ) -> None:
+        self.feature_i = feature_i
+        self.threshold = threshold
+        self.value = value
+        self.true_branch = true_branch
+        self.false_branch = false_branch
 
 
-# Super class of RegressionTree and ClassificationTree
-class ClassificationTree(object):
+class ClassificationTree:
     """Class of ClassificationTree.
-
-    Parameters:
-    -----------
-    min_samples_split: int
-        The minimum number of samples needed to make a split when building a tree.
-    min_impurity: float
-        The minimum impurity required to split the tree further.
-    max_depth: int
-        The maximum depth of a tree.
     """
 
-    def __init__(self, min_samples_split=2, min_impurity=1e-7, max_depth=float("inf")):
+    def __init__(
+        self,
+        min_samples_split: int = 2,
+        min_impurity: float = 1e-7,
+        max_depth: float = np.inf
+    ) -> None:
         self.root = None
         self.min_samples_split = min_samples_split
         self.min_impurity = min_impurity
         self.max_depth = max_depth
 
-    def fit(self, X, y):
-        """ Build decision tree """
-        self.root = self._build_tree(X, y)
+    def fit(
+        self,
+        x: np.ndarray,
+        y: np.ndarray
+    ):
+        self.root = self._build_tree(x, y)
 
-    def _build_tree(self, X, y, current_depth=0):
-        """ Recursive method which builds out the decision tree and splits X and respective y
-        on the feature of X which (based on impurity) best separates the data"""
-        largest_impurity = 0
-        best_criteria = {}    # Feature index and threshold
-        best_sets = {}        # Subsets of the data
+    def _build_tree(
+        self,
+        x: np.ndarray,
+        y: np.ndarray,
+        current_depth: int = 0
+    ):
+        """Recursive method which builds out the decision tree and splits
+        x and respective y on the feature of x which (based on impurity)
+        best separates the data.
+        """
+        largest_impurity = 0.0
+        best_criteria = {}
+        best_sets = {}
 
-        # Add y as last column of X
-        Xy = np.concatenate((X, y), axis=1)
-        n_samples, n_features = np.shape(X)
+        # Add y as last column of x
+        Xy = np.concatenate((x, y), axis=1)
+        n_samples, n_features = np.shape(x)
 
-        if n_samples >= self.min_samples_split and current_depth <= self.max_depth:
+        if (n_samples >= self.min_samples_split and
+                current_depth <= self.max_depth):
             # Calculate the impurity for each feature
             for feature_i in range(n_features):
                 # All values of feature_i
-                feature_values = X[:, feature_i]
+                feature_values = x[:, feature_i]
                 unique_values = np.unique(feature_values)
 
-                # Iterate through all unique values of feature column i and calculate the impurity
+                # Iterate through all unique values of feature column i and
+                # calculate the impurity
                 for threshold in unique_values:
-                    # Divide X and y depending on if the feature value of X at index feature_i
-                    # meets the threshold
+                    # Divide x and y depending on if the feature value
+                    # of x at index feature_i meets the threshold
                     Xy1, Xy2 = divide_on_feature(Xy, feature_i, threshold)
 
                     if len(Xy1) > 0 and len(Xy2) > 0:
@@ -136,36 +152,57 @@ class ClassificationTree(object):
                         # Calculate impurity
                         impurity = calculate_information_gain(y, y1, y2)
 
-                        # If this threshold resulted in a higher information gain than previously
-                        # recorded save the threshold value and the feature index
+                        # If this threshold resulted in a higher information
+                        # gain than previously recorded save the threshold value
+                        # and the feature index
                         if impurity > largest_impurity:
                             largest_impurity = impurity
-                            best_criteria = {"feature_i": feature_i, "threshold": threshold}
+                            best_criteria = {
+                                "feature_i": feature_i,
+                                "threshold": threshold
+                            }
                             best_sets = {
-                                "leftX": Xy1[:, :n_features],   # X of left subtree
-                                "lefty": Xy1[:, n_features:],   # y of left subtree
-                                "rightX": Xy2[:, :n_features],  # X of right subtree
-                                "righty": Xy2[:, n_features:]   # y of right subtree
+                                "x_left": Xy1[:, :n_features],
+                                "y_left": Xy1[:, n_features:],
+                                "x_right": Xy2[:, :n_features],
+                                "y_right": Xy2[:, n_features:]
                             }
 
         if largest_impurity > self.min_impurity:
             # Build subtrees for the right and left branches
-            true_branch = self._build_tree(best_sets["leftX"], best_sets["lefty"], current_depth + 1)
-            false_branch = self._build_tree(best_sets["rightX"], best_sets["righty"], current_depth + 1)
-            return DecisionNode(feature_i=best_criteria["feature_i"], threshold=best_criteria[
-                                "threshold"], true_branch=true_branch, false_branch=false_branch)
+            true_branch = self._build_tree(
+                best_sets["x_left"],
+                best_sets["y_left"],
+                current_depth + 1
+            )
+            false_branch = self._build_tree(
+                best_sets["x_right"],
+                best_sets["y_right"],
+                current_depth + 1
+            )
+            return DecisionNode(
+                feature_i=best_criteria["feature_i"],
+                threshold=best_criteria["threshold"],
+                true_branch=true_branch,
+                false_branch=false_branch
+            )
 
         # We're at leaf => determine value
         leaf_value = majority_vote(y)
         return DecisionNode(value=leaf_value)
 
-    def predict_value(self, x, tree=None):
-        """ Do a recursive search down the tree and make a prediction of the data sample by the
-            value of the leaf that we end up at """
+    def predict_value(
+        self,
+        x: np.ndarray,
+        tree: Optional[DecisionNode] = None
+    ):
+        """Do a recursive search down the tree and make a prediction of
+        the data sample by the value of the leaf that we end up at.
+        """
         if tree is None:
             tree = self.root
 
-        # If we have a value (i.e we're at a leaf) => return value as the prediction
+        # If we have a value (i.e we're at a leaf) => return value as the pred
         if tree.value is not None:
             return tree.value
 
@@ -180,28 +217,25 @@ class ClassificationTree(object):
         # Test subtree
         return self.predict_value(x, branch)
 
-    def predict(self, X):
-        """ Classify samples one by one and return the set of labels """
-        return [self.predict_value(sample) for sample in X]
+    def predict(
+        self,
+        x: np.ndarray
+    ):
+        y_pred = np.array([self.predict_value(xi) for xi in x])
+        return y_pred
 
-    def print_tree(self, tree=None, indent=" "):
-        """ Recursively print the decision tree """
-        if not tree:
-            tree = self.root
-
-        # If we're at leaf => print the label
-        if tree.value is not None:
-            print(tree.value)
-        # Go deeper down the tree
-        else:
-            # Print test
-            print("%s:%s? " % (tree.feature_i, tree.threshold))
-            # Print the true scenario
-            print("%sT->" % (indent), end="")
-            self.print_tree(tree.true_branch, indent + indent)
-            # Print the false scenario
-            print("%sF->" % (indent), end="")
-            self.print_tree(tree.false_branch, indent + indent)
+    def score(
+        self,
+        x: np.ndarray,
+        y: np.ndarray
+    ):
+        y_pred = self.predict(x)
+        true_pred = np.sum(
+            [y_pred_i == y_i for y_pred_i, y_i in zip(y_pred, y)]
+        )
+        n = len(y)
+        accuracy = true_pred / n
+        return accuracy
 
 
 if __name__ == "__main__":
@@ -211,5 +245,5 @@ if __name__ == "__main__":
     clf = ClassificationTree()
     clf.fit(x_train, y_train)
     y_pred = clf.predict(x_train)
-    print(y_train, y_pred)
-    clf.print_tree()
+    score = clf.score(x_train, y_train)
+    print(f"Accuracy: {score}")
