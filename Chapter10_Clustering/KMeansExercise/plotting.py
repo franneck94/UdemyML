390e406a84f7cb6cd578feba7b871c0ee0e2c28e
franneck94 <schaffranek.jan@web.de>
2021-11-27 00:29:44 +0100

    major update

--- Chapter10_Clustering/KMeansExercise/plotting.py
+++ Chapter10_Clustering/KMeansExercise/plotting.py
@@ -1,9 +1,12 @@
+import numpy as np
+np.random.seed(42)
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 cmap_light = ListedColormap(['#FFAAAA', '#AAAAFF', '#AAFFAA'])
+
 
 def make_meshgrid(x, y, h=.02):
     x_min, x_max = x.min() - 1, x.max() + 1
@@ -12,13 +15,14 @@ def make_meshgrid(x, y, h=.02):
                          np.arange(y_min, y_max, h))
     return xx, yy
 
+
 def plot_contours(ax, clf, xx, yy, **params):
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     out = ax.contourf(xx, yy, Z, **params)
     return out
 
-# Plot the data
+
 def plot(x, y, y_pred, centroids, kmeans):
     colors = ["red", "blue", "green"]
     fig, ax = plt.subplots()
@@ -27,6 +31,6 @@ def plot(x, y, y_pred, centroids, kmeans):
     xx, yy = make_meshgrid(X0, X1)
     plot_contours(ax, kmeans, xx, yy, cmap=cmap_light, alpha=0.8)
 
-    plt.scatter(x[:,0], x[:,1], c=y_pred)
-    plt.scatter(centroids[:,0], centroids[:,1], color="red", s=250, marker="*")
+    plt.scatter(x[:, 0], x[:, 1], c=y_pred)
+    plt.scatter(centroids[:, 0], centroids[:, 1], color="red", s=250, marker="*")
     plt.show()
---
+++ Chapter12_NN/LogisticRegression/plotting.py
@@ -0,0 +1,37 @@
+from typing import Any
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+cmap_light = ListedColormap(['#FFAAAA', '#AAAAFF'])
+colors = np.array(["red", "blue"])
+
+
+def plot_dataset(x: np.ndarray, y: np.ndarray) -> None:
+    plt.scatter(x[:, 0], x[:, 1], c=colors[y])
+    plt.show()
+
+
+def make_meshgrid(x: np.ndarray, y: np.ndarray, h: float = 0.02) -> Any:
+    x_min, x_max = x.min() - 1, x.max() + 1
+    y_min, y_max = y.min() - 1, y.max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                         np.arange(y_min, y_max, h))
+    return xx, yy
+
+
+def plot_contours(ax: Any, clf: Any, xx: np.ndarray, yy: np.ndarray, **params: Any) -> Any:
+    Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    out = ax.contourf(xx, yy, Z, **params)
+    return out
+
+
+def plot(clf: Any, x: np.ndarray, y: np.ndarray) -> None:
+    _, ax = plt.subplots()
+    X0, X1 = x[:, 0], x[:, 1]
+    xx, yy = make_meshgrid(X0, X1)
+    plot_contours(ax, clf, xx, yy, cmap=cmap_light, alpha=0.8)
+    plt.scatter(x[:, 0], x[:, 1], c=colors[y])
+    plt.show()
