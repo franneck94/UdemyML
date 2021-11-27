6e6c353bb445e82563d37d6dad17b28d6d089cfd
franneck94 <schaffranek.jan@web.de>
2021-11-27 11:55:44 +0100

    up

--- Chapter11_SVM/SVMSklearn/plotting.py
+++ Chapter11_SVM/SVMSklearn/plotting.py
@@ -1,20 +1,19 @@
+from typing import Any
+
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+cmap_light = ListedColormap(['#FFAAAA', '#AAAAFF'])
+colors = np.array(["red", "blue"])
 
 
-def make_meshgrid(x, y, h=.02):
-    """Create a mesh of points to plot in
+def plot_dataset(x: np.ndarray, y: np.ndarray) -> None:
+    plt.scatter(x[:, 0], x[:, 1], c=colors[y])
+    plt.show()
 
-    Parameters
-    ----------
-    x: data to base x-axis meshgrid on
-    y: data to base y-axis meshgrid on
-    h: stepsize for meshgrid, optional
 
-    Returns
-    -------
-    xx, yy : ndarray
-    """
+def make_meshgrid(x: np.ndarray, y: np.ndarray, h: float = 0.02) -> Any:
     x_min, x_max = x.min() - 1, x.max() + 1
     y_min, y_max = y.min() - 1, y.max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
@@ -22,19 +21,17 @@ def make_meshgrid(x, y, h=.02):
     return xx, yy
 
 
-def plot_contours(ax, clf, x, y, **params):
-    """Plot the decision boundaries for a classifier.
-
-    Parameters
-    ----------
-    ax: matplotlib axes object
-    clf: a classifier
-    xx: meshgrid ndarray
-    yy: meshgrid ndarray
-    params: dictionary of params to pass to contourf, optional
-    """
-    xx, yy = make_meshgrid(x, y, h=.02)
+def plot_contours_(ax: Any, clf: Any, xx: np.ndarray, yy: np.ndarray, **params: Any) -> Any:
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     out = ax.contourf(xx, yy, Z, **params)
     return out
+
+
+def plot_contours(clf: Any, x: np.ndarray, y: np.ndarray) -> None:
+    _, ax = plt.subplots()
+    X0, X1 = x[:, 0], x[:, 1]
+    xx, yy = make_meshgrid(X0, X1)
+    plot_contours_(ax, clf, xx, yy, cmap=cmap_light, alpha=0.8)
+    plt.scatter(x[:, 0], x[:, 1], c=colors[y])
+    plt.show()
