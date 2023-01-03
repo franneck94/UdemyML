cbb7db999afab4c68ed38d09556b881b883029f0
franneck94 <schaffranek.jan@web.de>
2023-01-03 13:34:57 +0100

    up

--- Chapter10_Clustering/GaussianCLusteringSklearn/plotting.py
+++ Chapter10_Clustering/GaussianCLusteringSklearn/plotting.py
@@ -1,9 +1,9 @@
 from typing import Any
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy import linalg
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 
 
 colors = ["yellow", "purple"]
@@ -16,28 +16,29 @@ def plot_results(
     means: np.ndarray,
     covariances: np.ndarray,
     index: int,
-    title: Any
+    title: Any,
 ) -> None:
     _ = plt.figure(figsize=(12, 12))
     splot = plt.subplot(2, 1, 1 + index)
-    for i, (mean, covar) in enumerate(zip(
-            means, covariances)):
+    for i, (mean, covar) in enumerate(zip(means, covariances)):
         v, w = linalg.eigh(covar)
-        v = 2. * np.sqrt(2.) * np.sqrt(v)
+        v = 2.0 * np.sqrt(2.0) * np.sqrt(v)
         u = w[0] / linalg.norm(w[0])
         if not np.any(Y_ == i):
             continue
         plt.scatter(X[Y_ == i, 0], X[Y_ == i, 1], color=colors[i], s=20)
 
         angle = np.arctan(u[1] / u[0])
-        angle = 180. * angle / np.pi
-        ell = mpl.patches.Ellipse(mean, v[0], v[1], 180. + angle, color=colors[i])
+        angle = 180.0 * angle / np.pi
+        ell = mpl.patches.Ellipse(
+            mean, v[0], v[1], 180.0 + angle, color=colors[i]
+        )
         ell.set_clip_box(splot.bbox)
         ell.set_alpha(0.5)
         splot.add_artist(ell)
 
-    plt.xlim(-5., 5.)
-    plt.ylim(-5., 5.)
+    plt.xlim(-5.0, 5.0)
+    plt.ylim(-5.0, 5.0)
     plt.xticks(())
     plt.yticks(())
     plt.title(title)
