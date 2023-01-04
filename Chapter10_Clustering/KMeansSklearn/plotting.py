c018da5e266090bc32300230939133ec7934d7fd
franneck94 <schaffranek.jan@web.de>
2023-01-04 09:08:13 +0100

    up

--- Chapter10_Clustering/KMeansSklearn/plotting.py
+++ Chapter10_Clustering/KMeansSklearn/plotting.py
@@ -1,27 +1,42 @@
-import matplotlib.pyplot as plt
+from typing import Any
+from typing import Tuple
+
 import numpy as np
+
+
+np.random.seed(42)
+import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
+from sklearn.cluster import KMeans
 
 
 cmap_light = ListedColormap(["#FFAAAA", "#AAAAFF", "#AAFFAA"])
 
 
-def make_meshgrid(x, y, h=0.02):
+def make_meshgrid(
+    x: np.ndarray, y: np.ndarray, h: float = 0.02
+) -> Tuple[np.ndarray, np.ndarray]:
     x_min, x_max = x.min() - 1, x.max() + 1
     y_min, y_max = y.min() - 1, y.max() + 1
     xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
     return xx, yy
 
 
-def plot_contours(ax, clf, xx, yy, **params):
+def plot_contours(
+    ax: plt.Axes, clf: KMeans, xx: np.ndarray, yy: np.ndarray, **params: Any
+) -> Any:
     Z = clf.predict(np.c_[xx.ravel(), yy.ravel()])
     Z = Z.reshape(xx.shape)
     out = ax.contourf(xx, yy, Z, **params)
     return out
 
 
-# Plot the data
-def plot(x, y, y_pred, centroids, kmeans):
+def plot(
+    x: np.ndarray,
+    y_pred: np.ndarray,
+    centroids: np.ndarray,
+    kmeans: np.ndarray,
+) -> None:
     _, ax = plt.subplots()
     # Decision Border
     X0, X1 = x[:, 0], x[:, 1]
